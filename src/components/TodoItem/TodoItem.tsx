eb60d10be9fc2a926569b99c95b4fed013323293
dayoolacodes <dayo.ayoola@four.agency>
2025-01-07 00:29:43 +0000

    fadeout on delete

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { toggleTodo, deleteTodo } from "../../redux/todoSlice";
 import classes from "./TodoItem.module.css";
@@ -11,16 +11,27 @@ interface TodoItemProps {
 
 const TodoItem: React.FC<TodoItemProps> = ({ id, text, completed }) => {
   const dispatch = useDispatch();
+  const [isFading, setIsFading] = useState(false);
+
+  const handleDeleteTodo = () => {
+    setIsFading(true);
+    setTimeout(() => {
+      dispatch(deleteTodo(id)); 
+    }, 800);
+  };
 
   return (
-    <li className={classes.list}>
+    <li className={`${classes.list} ${isFading ? classes.fadeout : ""}`}>
       <span
-        style={{ textDecoration: completed ? "line-through" : "none", cursor: "pointer" }}
+        style={{
+          textDecoration: completed ? "line-through" : "none",
+          cursor: "pointer"
+        }}
         onClick={() => dispatch(toggleTodo(id))}
       >
         {text}
       </span>
-      <button onClick={() => dispatch(deleteTodo(id))}>Remove</button>
+      <button onClick={handleDeleteTodo}>Remove</button>
       {completed ? <span> done ✔️ </span> : <span />}
     </li>
   );
